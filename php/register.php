3f6d21f114c42db388142483bb847a66a0a7065b
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-13 18:58:39 -0400

    Add inputs to form

--- php/register.php
+++ php/register.php
@@ -1,6 +1,10 @@
 <?php
   $homePath = '../';
   $pagePath = './';
+
+  if (isset($_POST['register'])) {
+    echo $_POST['firstname'];
+  }
 ?>
 
 <!DOCTYPE html>
@@ -16,13 +20,37 @@
   <?php include('../templates/header.php') ?>
   <main>
     <h1>Register</h1>
-    <form>
-      <label>First Name: <input type="text" name="firstname"></label>
-      <label>Last Name: <input type="text" name="lastname"></label>
-      <label>Email: <input type="email" name="email"></label>
-      <label>Username: <input type="text" name="username"></label>
-      <label>Password: <input type="password" name="pword"></label>
-      <label>Confirm Password: <input type="password" name="confirmpword"></label>
+    <form action="register.php" method="POST">
+      <fieldset>
+        <legend>Personal Info</legend>
+        <div>
+          <label>First Name: <input type="text" name="firstname"></label>
+        </div>
+        <div>
+          <label>Last Name: <input type="text" name="lastname"></label>
+        </div>
+        <div>
+          <label>Email: <input type="email" name="email"></label>
+        </div>
+        <div>
+          <span>Gender:</span>
+          <label><input type="radio" name="gender" value="Female">Female</label>
+          <label><input type="radio" name="gender" value="Male">Male</label>
+          <label><input type="radio" name="gender" value="Non-binary">Non-binary</label>
+          <label><input type="radio" name="gender" value="Prefer not to say">Prefer not to say</label>
+      </fieldset>
+      <fieldset>
+        <legend>Account Info</legend>
+        <div>
+          <label>Username: <input type="text" name="username"></label>
+        </div>
+        <div>
+          <label>Password: <input type="password" name="pword"></label>
+        </div>
+        <div>
+          <label>Confirm Password: <input type="password" name="confirmpword"></label>
+        </div>
+      </fieldset>
       <input type="submit" name="register" value="Register">
     </form>
   </main>
--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
     <ul>
       <li><a href="<?php echo $homePath.'index.php' ?>">Home</a></li>
       <li><a href="<?php echo $pagePath.'about.php' ?>">About Us</a></li>
-      <li><a href="<?php echo $pagePath.'attractions.php' ?>">Featured Attractions</a></li>
+      <li><a href="<?php echo $pagePath.'attractions.php' ?>">Attractions</a></li>
       <li><a href="<?php echo $pagePath.'login.php' ?>">Log in</a></li>
       <li><a href="<?php echo $pagePath.'register.php' ?>">Register</a></li>
     </ul>
