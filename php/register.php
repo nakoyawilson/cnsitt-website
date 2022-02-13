1e535db77c91efbba5be3e20d4c59ac8003b96e9
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-13 17:12:18 -0400

    Add a form to register an account

--- php/register.php
+++ php/register.php
@@ -16,6 +16,15 @@
   <?php include('../templates/header.php') ?>
   <main>
     <h1>Register</h1>
+    <form>
+      <label>First Name: <input type="text" name="firstname"></label>
+      <label>Last Name: <input type="text" name="lastname"></label>
+      <label>Email: <input type="email" name="email"></label>
+      <label>Username: <input type="text" name="username"></label>
+      <label>Password: <input type="password" name="pword"></label>
+      <label>Confirm Password: <input type="password" name="confirmpword"></label>
+      <input type="submit" name="register" value="Register">
+    </form>
   </main>
   <?php include('../templates/footer.php') ?>
 </body>
