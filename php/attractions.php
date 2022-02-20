d08d4de63c86b4095516845bf90757580f4bb5bc
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-20 12:43:41 -0400

    Update page title

--- php/attractions.php
+++ php/attractions.php
@@ -9,13 +9,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Featured Attractions</title>
+  <title>Attractions</title>
   <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
   <?php include('../templates/header.php') ?>
   <main>
-    <h1>Featured Attractions</h1>
+    <h1>Attractions</h1>
   </main>
   <?php include('../templates/footer.php') ?>
 </body>
