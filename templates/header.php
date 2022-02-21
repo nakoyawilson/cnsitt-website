4ddb2e5f8d0866b8bfe7f7cb2b466f78bb543b9d
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-21 11:42:29 -0400

    Conditionally render links

--- templates/header.php
+++ templates/header.php
@@ -4,8 +4,18 @@
       <li><a href="<?php echo $homePath.'index.php' ?>">Home</a></li>
       <li><a href="<?php echo $pagePath.'about.php' ?>">About Us</a></li>
       <li><a href="<?php echo $pagePath.'attractions.php' ?>">Attractions</a></li>
-      <li><a href="<?php echo $pagePath.'login.php' ?>">Log in</a></li>
-      <li><a href="<?php echo $pagePath.'register.php' ?>">Register</a></li>
+      <?php
+        if(isset($_SESSION['user']))
+        {
+          echo '<li><a href="'.$pagePath.'add_attractions.php">Add Attraction</a></li>';
+          echo '<li><a href="'.$pagePath.'update_profile.php">Update Profile</a></li>';
+          echo '<li><a href="'.$pagePath.'logout.php">Log out</a></li>';
+        }
+        else{
+          echo '<li><a href="'.$pagePath.'login.php">Log in</a></li>';
+          echo '<li><a href="'.$pagePath.'register.php">Register</a></li>';
+        }
+      ?>
     </ul>
   </nav>
   <form action="">
